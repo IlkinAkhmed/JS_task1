7966642275073eb2f7cf859497ab555e14d07b33
IlkinAkhmed <tu7mp4w99@code.edu.az>
2023-10-11 23:52:08 +0400

    all task was created

---
+++ script.js
@@ -0,0 +1,33 @@
+// 1den 100e qeder 3e ve 5e bolunen ededlerin cemini hesablayan program
+
+// let a = 1
+// let cem = 0
+// while(a<100){
+//     a++;
+//     if (a%3===0 || a%5===0){
+//         cem+=a;
+//     }
+// }
+// console.log(cem);
+
+
+
+// verilen ededin 2nin quvveti olub olmadigini yoxlayan program
+
+// let eded = 8
+// while(eded>1){
+//     if (eded%2!==0){
+//         console.log('bu eded 2nin quvveti deyil');
+//     } else{
+//         eded/=2
+//     }
+//     if(eded===1){
+//         console.log('bu eded 2nin quvetidir');
+//     }
+     
+// }
+
+
+
+
+
